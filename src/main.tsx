c59499fb1d2405ca39f9e7b45d4ea209d9dd82d9
naman1326 <ghodakenaman20@gmail.com>
2026-09-07 02:19:09 +0530

    fix: set basename to /ffcs-admin in BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ffcs-admin">
       <AuthProvider>
         <App />
         <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
